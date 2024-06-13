8f09c2a52f16e43e5b9b19a7bc215cbb50398963
zsline <sergei.dereka@gmail.com>
2024-06-13 21:08:22 +0300

    13-06-24-21

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,64 @@
-const swiper = new Swiper('.swiper-product', {
-    // Optional parameters
-    loop: true,
-    autoplay: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-product-pagination',
-      clickable:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-product-button-next',
-      prevEl: '.swiper-product-button-prev',
-    },
-  
+document.addEventListener('DOMContentLoaded', () => {
+  const heroCenter = window.screen.height / 2;
+  const main = document.querySelector('.main');
+  const header = document.querySelector('.header');
+  const scrollItems = document.querySelectorAll('.scroll-item');
+
+  // ========== functions ================
+  const headerFixed = () => {
+    let scrollTop = window.scrollY;
+    if (scrollTop >= heroCenter) {
+      header.classList.add('fixed');
+      main.style.marginTop = `${header.offsetHeight}px`;
+    } else {
+      header.classList.remove('fixed');
+      main.style.marginTop = `0px`;
+    }
+  }
+
+  const scrollAnimation = () => {
+    let windowCenter = (window.innerHeight / 2) + window.scrollY;
+    if (scrollItems) {
+      scrollItems.forEach(el => {
+        let scrollOffset = el.offsetTop;
+
+        if (windowCenter >= scrollOffset) {
+          el.classList.add('animate');
+        } else {
+          el.classList.remove('animate');
+        }
+      });
+    }
+
+  }
+  // ========== end functions ================  
+
+  headerFixed();
+  window.addEventListener('scroll', () => {
+    headerFixed();
+    scrollAnimation();
   });
+
+  if (document.querySelector('.swiper-product')) {
+    const swiper = new Swiper('.swiper-product', {
+      // Optional parameters
+      loop: true,
+      autoplay: true,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-product-pagination',
+        clickable: true,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-product-button-next',
+        prevEl: '.swiper-product-button-prev',
+      },
+
+    });
+  }
+
+});
+
+

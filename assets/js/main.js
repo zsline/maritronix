d8ed68972f9a2c42b32fce21270c8ca6e2bfb292
zsline <sergei.dereka@gmail.com>
2024-06-15 14:45:10 +0300

    15-06-24-14

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 document.addEventListener('DOMContentLoaded', () => {
   const heroCenter = window.screen.height / 2;
   const main = document.querySelector('.main');
@@ -57,6 +58,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+
+  // ==============  Hidden Company Input 
+
+
   const inputProperty = document.querySelectorAll('[name="billing-property"]');
   const inputShippingProperty = document.querySelectorAll('[name="shipping-property"]');
   const companyArt = document.querySelectorAll('.company-atr');
@@ -66,10 +71,13 @@ document.addEventListener('DOMContentLoaded', () => {
       if (el.value == 'individual-costomer') {
         companyArt.forEach(el => {
           el.classList.add('hidden');
+          el.children[1].classList.remove('_req');
+          console.dir(el);
         })
       } else {
         companyArt.forEach(el => {
           el.classList.remove('hidden');
+          el.children[1].classList.add('_req');
         })
       }
     });
@@ -79,15 +87,18 @@ document.addEventListener('DOMContentLoaded', () => {
       if (el.value == 'individual-costomer') {
         companyShippingArt.forEach(el => {
           el.classList.add('hidden');
+          el.children[1].classList.remove('_req');
         })
       } else {
         companyShippingArt.forEach(el => {
           el.classList.remove('hidden');
+          el.children[1].classList.add('_req');
         })
       }
     });
   });
 
+// ===================  Billing VS Shipping
 
   const buttonShipping = document.querySelector('.button-shipping');
   const buttonBilling = document.querySelector('.button-billing');
@@ -106,6 +117,68 @@ document.addEventListener('DOMContentLoaded', () => {
     buttonBilling.classList.toggle('hidden');
   });
 
+
+
+  // ================== FORM VALIDATE ===========
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+    let formData = new FormData(form);
+
+
+
+    if(error === 0){
+      document.querySelector('.main').classList.add('_sending');
+      // let response = await fetch('sendmail.php', {
+      //   method: 'POST',
+      //   body: formData
+      // });
+      // if(response.ok){
+      //   let result = await response.json();
+      //   alert(result.message);
+      //   form.reset();
+
+      // } else {
+      //   alert('Error');
+      // }
+      // console.log(formData);
+    } else {
+      console.log(error);
+    }
+  }
+
+  function formValidate(form){
+    let error = 0;
+    
+    let formReq = document.querySelectorAll('._req');
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if(input.value == ''){
+        billing.classList.remove('hidden');
+        buttonBilling.classList.add('hidden');
+        formAddError(input);
+        error++;
+        
+      }
+    }
+    return error;
+
+  }
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
 });
 
 
